d05dd9f6db7cadd28c4ea51ddd628c807c2230ce
mrome007 <mrome007@ucr.edu>
2019-05-16 22:51:59 -0700

    Add card assets and create a button in the inspector to populate the deck of cards.

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField]
+    private Image cardImage;
+
+    [SerializeField]
+    private Suit cardSuit;
+
+    [SerializeField]
+    protected int cardValue;
+
+    public Suit CardSuit { get { return cardSuit; } set { cardSuit = value; } }
+    public virtual int CardValue { get { return cardValue; } set { cardValue = value; } }
+    public Image CardImage { get { return cardImage; } }
+}
+
+public enum Suit
+{
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+}
---
+++ Assets/Scripts/Cards/Editor/DeckPopulatorEditor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[CustomEditor(typeof(DeckPopulator))]
+public class DeckPopulatorEditor : Editor
+{
+    private DeckPopulator deckPopulator;
+
+    private void OnEnable()
+    {
+        deckPopulator = target as DeckPopulator;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if(deckPopulator == null)
+        {
+            deckPopulator = target as DeckPopulator;
+        }
+
+        DrawPopulateDeck();
+
+        if(GUI.changed)
+        {
+            EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+        }
+    }
+
+    private void DrawPopulateDeck()
+    {
+        if(GUILayout.Button("Populate Deck"))
+        {
+            GUI.changed = PopulateDeck();
+        }
+    }
+
+    private bool PopulateDeck()
+    {
+        Suit[] suits = (Suit[])Enum.GetValues(typeof(Suit));
+        var suitIndex = 0;
+        var currentSuit = suits[suitIndex];
+        var cardCount = 0;
+
+        var cardImages = Resources.LoadAll<Sprite>("Cards");
+        var cardImagesIndex = 0;
+
+        foreach(Transform child in deckPopulator.transform)
+        {
+            if (cardCount >= 13)
+            {
+                suitIndex++;
+                currentSuit = suits[suitIndex];
+                cardCount = 0;
+            }
+
+            var card = child.GetComponent<Card>();
+            card.CardValue = cardCount + 1;
+            card.CardSuit = currentSuit;
+            card.CardImage.sprite = cardImages[cardImagesIndex];
+            card.gameObject.name = GetCardName(card.CardValue) + "Of" + card.CardSuit.ToString() + "s";
+
+            cardCount++;
+            cardImagesIndex++;
+        }
+        return true;
+    }
+
+    private string GetCardName(int value)
+    {
+        var cardName = string.Empty;
+
+        switch(value)
+        {
+            case 1:
+                cardName = "Ace";
+                break;
+            case 11:
+                cardName = "Jack";
+                break;
+            case 12:
+                cardName = "Queen";
+                break;
+            case 13:
+                cardName = "King";
+                break;
+
+            default:
+                cardName = value.ToString();
+                break;
+        }
+
+        return cardName;
+    }
+}
