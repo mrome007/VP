012d9722e1930950fde977346f7ebc6b3d0b22aa
mrome007 <mrome007@ucr.edu>
2019-05-16 23:45:00 -0700

    Setting up state machine system for video poker game.

---
+++ Assets/Scripts/States/PresentationState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentationState : MonoBehaviour
+{
+    public virtual void EnterState()
+    {
+
+    }
+
+    public virtual void ExitState()
+    {
+
+    }
+}
