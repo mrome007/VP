8103c5c0c2bcae34c24c8d7629ab3343cfef9a75
mrome007 <mrome007@ucr.edu>
2019-05-16 23:34:27 -0700

    Create Shuffle utilty class.

--- Assets/Scripts/Cards/Deck.cs
+++ Assets/Scripts/Cards/Deck.cs
@@ -6,4 +6,18 @@ public class Deck : MonoBehaviour
 {
     [SerializeField]
     private List<Card> cards;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CardShuffle.Shuffle(cards);
+
+            for(var index = 0; index < cards.Count; index++)
+            {
+                var card = cards[index];
+                card.transform.SetSiblingIndex(index);
+            }
+        }
+    }
 }
